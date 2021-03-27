57f3c559cd111b02fe39c77b377b492c12b44a79
PtCookie <ptcookie@outlook.com>
2021-03-27 22:40:00 +0900

    feat: Practice
    Practice for Microsoft Learn.

---
+++ CookieConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Humanizer;
+
+namespace CookieConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantities:");
+            HumanizeQuantities();
+            
+            Console.WriteLine("\nDate/Time Manipulation:");
+            HumanizeDates();
+        }
+
+        private static void HumanizeQuantities()
+        {
+            Console.WriteLine("case".ToQuantity(0));
+            Console.WriteLine("case".ToQuantity(1));
+            Console.WriteLine("case".ToQuantity(5));
+        }
+
+        private static void HumanizeDates()
+        {
+            Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+            Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+            Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+            Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+        }
+    }
+}
